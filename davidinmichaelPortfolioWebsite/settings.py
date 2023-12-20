efc12d77cf44caa165d068c7e9cf75122d032943
Matthew Ndoh <matthewndoh@gmail.com>
2023-12-20 09:45:34 +0100

    Django reload set up complete

--- davidinmichaelPortfolioWebsite/settings.py
+++ davidinmichaelPortfolioWebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cn6arx9+*lg^cn!#3&d+)@!n)ymps*w*vkd+kplm)kxcdcw8+1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "django_browser_reload",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -47,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = 'davidinmichaelPortfolioWebsite.urls'
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'davidinmichaelPortfolioWebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
